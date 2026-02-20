7d94e3535db0eedecc2dfc22d75952042e2d8190
Alberto Balderas <abalderas10@gmail.com>
2026-02-20 17:45:37 -0600

    Hero principal con video 0220(1) comprimido

--- src/views/landings/default/data/hero.jsx
+++ src/views/landings/default/data/hero.jsx
@@ -14,5 +14,5 @@ export const hero = {
   caption: 'Soluciones financieras ágiles y flexibles para personas y negocios en México.',
   secondaryBtn: { children: 'Solicitar crédito ahora', href: '/auth/registro' },
   poster: { light: '/assets/images/capitalta/hero-main.jpeg', dark: '/assets/images/capitalta/hero-main.jpeg' },
-  videoSrc: '/assets/videos/test.mp4'
+  videoSrc: '/assets/0220(1).mp4'
 };
