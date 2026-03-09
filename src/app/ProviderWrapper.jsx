78e40e4bfd5b093db8208a9cb4fe54b1329f962f
Alberto Balderas <abalderas10@gmail.com>
2026-03-09 08:51:02 -0600

    Add AuthProvider to ProviderWrapper to fix runtime error

--- src/app/ProviderWrapper.jsx
+++ src/app/ProviderWrapper.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 import { ConfigProvider } from '@/contexts/ConfigContext';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 /***************************  COMMON - CONFIG, THEME  ***************************/
 
 export default function ProviderWrapper({ children }) {
-  return <ConfigProvider>{children}</ConfigProvider>;
+  return (
+    <ConfigProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </ConfigProvider>
+  );
 }
 
 ProviderWrapper.propTypes = { children: PropTypes.any };
