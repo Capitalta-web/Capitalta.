c8ab46903dd93b549095841b91a3fc1748e3447f
Alberto Balderas <abalderas10@gmail.com>
2026-03-18 18:01:39 -0600

    ui: remove whatsapp and secondary header buttons

--- src/views/landings/default/data/navbar.jsx
+++ src/views/landings/default/data/navbar.jsx
@@ -4,13 +4,6 @@
 
 export const navbar = {
   customization: false,
-  whatsappBtn: {
-    children: '',
-    href: 'https://wa.me/525652016445?text=Hola%20Capitalta%2C%20quiero%20informaci%C3%B3n%20sobre%20un%20cr%C3%A9dito.',
-    target: '_blank',
-    sx: { minWidth: 40, width: 40, px: 0 }
-  },
-  secondaryBtn: { children: 'Brokers', href: '/brokers' },
   primaryBtn: { children: 'Solicitar crédito', href: '/auth/registro' },
   animated: true,
   navItems: [
